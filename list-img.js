1f4c488517b96bacc290b629f08a25dc588d7682
Edit Mr <elvismao.070512@gmail.com>
2023-11-20 00:56:55 +0800

    📋列出照片檔案

--- list-img.js
+++ list-img.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const imgFolder = './img';  // Adjust the path accordingly
+const imgFolder = './img';
 const jsonFilePath = 'images.json';
 
 function readFilesInFolder(folderPath) {
